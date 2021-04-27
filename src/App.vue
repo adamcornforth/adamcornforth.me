26cf75bc83a218965aa339a1f7eec0c1cce1a3bb
Adam Cornforth <adam@apcdesigner.com>
2021-04-27 20:43:16 +0100

    Fix component import

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app>
-      <v-img
-            src="harpur-hill.jpg"
-            gradient="to bottom, rgba(203,58,250,.3), rgba(0, 0, 0,.6)"
-      >
-          <Me />
-      </v-img>
+    <v-img
+      src="harpur-hill.jpg"
+      gradient="to bottom, rgba(203,58,250,.3), rgba(0, 0, 0,.6)"
+    >
+      <Me />
+    </v-img>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Me from "@/components/Me";
+import Me from "@/components/Me.vue";
 
 export default Vue.extend({
   name: "App",
